9e447d3de3e619d81bafc5af72d21ec458f0e172
renegade <renegadethousand@gmail.com>
2022-03-07 11:25:03 +0300

    Section 3. notification

--- src/main/java/BankTransaction.java
+++ src/main/java/BankTransaction.java
@@ -1,4 +1,5 @@
 import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
 import java.util.Objects;
 
 public class BankTransaction {
@@ -22,6 +23,31 @@ public class BankTransaction {
 
     public String getDescription() {
         return description;
+    }
+
+    public Notification validate() {
+        final Notification notification = new Notification();
+        if (this.description.length() > 100) {
+            notification.addError("The description is too long");
+        }
+
+        final LocalDate parsedDate;
+        try {
+            parsedDate = LocalDate.parse(this.date.toString());
+            if (parsedDate.isAfter(LocalDate.now())) {
+                notification.addError("date cannot be in the future");
+            }
+        } catch (DateTimeParseException exception) {
+            notification.addError("Invalid format for date");
+        }
+
+        final double amount;
+        try {
+            amount = Double.parseDouble(String.valueOf(this.amount));
+        } catch (NumberFormatException exception) {
+            notification.addError("Invalid format for amount");
+        }
+        return notification;
     }
 
     @Override
