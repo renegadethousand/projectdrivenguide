65796622ef8d6abd4fad8502d574870a64db9c5c
renegade <renegadethousand@gmail.com>
2022-03-07 10:12:39 +0300

    add functional interface

--- src/main/java/BankStatementProcessor.java
+++ src/main/java/BankStatementProcessor.java
@@ -38,6 +38,16 @@ public class BankStatementProcessor {
         return total;
     }
 
+    public List<BankTransaction> findTransactions(final BankTransactionFilter bankTransactionFilter) {
+        final List<BankTransaction> result = new ArrayList<>();
+        for (final BankTransaction bankTransaction: bankTransactions) {
+            if (bankTransactionFilter.test(bankTransaction)) {
+                result.add(bankTransaction);
+            }
+        }
+        return result;
+    }
+
     public List<BankTransaction> findTransactionsGreaterThanEqual(final int amount) {
         final List<BankTransaction> result = new ArrayList<>();
         for (final BankTransaction bankTransaction: bankTransactions) {
@@ -53,6 +63,17 @@ public class BankStatementProcessor {
         final List<BankTransaction> result = new ArrayList<>();
         for (final BankTransaction bankTransaction : bankTransactions) {
             if (bankTransaction.getDate().getMonth() == month) {
+                result.add(bankTransaction);
+            }
+        }
+        return result;
+    }
+
+    public List<BankTransaction> findTransactionsInMonthAndGreater(final Month month, final int amount) {
+
+        final List<BankTransaction> result = new ArrayList<>();
+        for (final BankTransaction bankTransaction : bankTransactions) {
+            if (bankTransaction.getDate().getMonth() == month && bankTransaction.getAmount() >= amount) {
                 result.add(bankTransaction);
             }
         }
